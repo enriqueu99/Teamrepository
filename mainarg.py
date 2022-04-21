3b99a0c11d742e20e4b9ccce716c8269485541b3
enriqueu99 <98370858+enriqueu99@users.noreply.github.com>
2022-04-21 16:26:32 -0400

    Update mainarg.py

--- mainarg.py
+++ mainarg.py
@@ -1,1 +1,51 @@
 #Web-APP that identifies proper market entrances
+import yfinance as yf
+
+
+
+msft = yf.Ticker("MSFT")
+
+
+
+
+def stock(ticker):
+    share = yf.Ticker(ticker)
+    #now a callable dictionary, need to look at ouput for keys 
+    b = share.info
+    
+    
+    return 'day high',b['dayHigh'],'52 week low',b['fiftyTwoWeekLow'],'regular market open',b['regularMarketOpen']
+    
+
+#modify ticker to change output
+x = stock('AAPL')
+zzz = stock('MSFT')
+xxx = stock('TSLA')
+nnn = stock('NFLX')
+
+
+def convertlst(a):
+    it = iter(a)
+    res_dct = dict(zip(it, it))
+    return res_dct
+#Endpoint whereby the data has been converted 
+z = convertlst(x)#AAPL
+xx = convertlst(zzz)#MSFT
+
+
+def stockanalysis(z):
+    bb = z['day high']
+    bb = int(bb) *45 
+    return 
+
+#print(stockanalysis(z))
+
+
+#Testing output 
+print(stock('AAPL'))
+#succesfully converted list into dictionary 
+
+#print(z)
+
+
+
